d4e029e213b4a0612b207638b97534c6a52b2ae3
booey2121 <booey2121@gmail.com>
2024-09-02 10:54:06 +1000

    Added: Player sprite, player movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,12 +9,37 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 	print("Starting asteroids!")
 
+	game_clock = pygame.time.Clock()
+	dt = 0  # delta_time
+
+	group_updatable = pygame.sprite.Group()
+	group_drawable = pygame.sprite.Group()
+
+	Player.containers = (group_updatable, group_drawable)
+
+	Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # removed obj bc using groups
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill("black")
+
+		screen.fill((0, 0, 0))
+
+		# my_player.draw(screen)
+		# my_player.update(dt)
+
+		for obj in group_updatable:
+			obj.update(dt)
+
+		for obj in group_drawable:
+			obj.draw(screen)
+
 		pygame.display.flip()
+
+		amount_time = game_clock.tick(60)
+		dt = amount_time / 1000
+
 
 if __name__ == "__main__":
 	main()
